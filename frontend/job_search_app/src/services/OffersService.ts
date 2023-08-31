31c94340e566bfd5a93186832859bd91fe44929a
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-31 14:54:36 +0200

    Created service for communication with backend

---
+++ frontend/job_search_app/src/services/OffersService.ts
@@ -0,0 +1,33 @@
+import { Offer } from '@my_types/Offer';
+import axios, { AxiosInstance } from 'axios';
+
+class OfferService {
+  private http: AxiosInstance;
+
+  constructor() {
+    this.http = axios.create({
+      baseURL: import.meta.env.VITE_API,
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+  }
+
+  async getAllOffers(): Promise<Offer[] | undefined> {
+    try {
+      return (await this.http.get('/offers')).data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async getAllSkills(): Promise<Offer[] | undefined> {
+    try {
+      return (await this.http.get('/skills')).data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+export default OfferService;
