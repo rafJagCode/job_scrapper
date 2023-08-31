8f0ab17b055043ddf9bd9cbe48fbf9aac5ad047d
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-31 14:51:07 +0200

    Added cors to backend

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { readFile } from '../helpers/file.js';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/offers', async (req, res, next) => {
   const offers = await readFile('100_offers.json');
