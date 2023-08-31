c6a70dc7832b860edcf0a64773d19e3879a644bd
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-31 14:49:54 +0200

    Configurated frontend

---
+++ frontend/job_search_app/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/scss/variables.scss";`,
+      },
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
+      '@my_types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+    },
+  },
+});
