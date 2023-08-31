336816e7d3d6d8d786bda7aa6f91c605a6a26983
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-31 14:55:11 +0200

    Added experience level component

---
+++ frontend/job_search_app/src/components/ExperienceLevel.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ExperienceLevel } from '@/types/Offer';
+
+const props = defineProps<{
+  experience_level: ExperienceLevel;
+}>();
+</script>
+
+<template>
+  <div class="experience_level" :data-experience-level="experience_level">{{ experience_level.toUpperCase() }}</div>
+</template>
+
+<style lang="scss">
+.experience_level {
+  font-size: 1.2rem;
+  &[data-experience-level='junior'] {
+    color: $low;
+  }
+  &[data-experience-level='mid'] {
+    color: $medium;
+  }
+  &[data-experience-level='senior'] {
+    color: $high;
+  }
+}
+</style>
