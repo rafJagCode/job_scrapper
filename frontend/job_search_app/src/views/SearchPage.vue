96cb37f73fdd60561df23184d974a2550ce0f477
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-31 14:53:45 +0200

    Added main page

---
+++ frontend/job_search_app/src/views/SearchPage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import JobOffer from '@components/JobOffer.vue';
+import OffersService from '@services/OffersService';
+import { Offer } from '@my_types/Offer';
+import { ref, onMounted } from 'vue';
+
+const offers = ref<Offer[]>();
+
+onMounted(async () => {
+  offers.value = await new OffersService().getAllOffers();
+});
+</script>
+
+<template>
+  <main class="search_page">
+    <JobOffer v-for="(offer, index) in offers" :key="index" :offer="offer" />
+  </main>
+</template>
+
+<style lang="scss">
+.search_page {
+  width: 100%;
+  height: 100%;
+  background-color: $primary;
+  overflow: auto;
+}
+</style>
