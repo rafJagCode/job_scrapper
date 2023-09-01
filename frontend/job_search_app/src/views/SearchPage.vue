bf85b8aac60b30cf91231dfa2dbf1260a683d4be
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-01 17:58:51 +0200

    Added fetching skill list

--- frontend/job_search_app/src/views/SearchPage.vue
+++ frontend/job_search_app/src/views/SearchPage.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import JobOffer from '@components/JobOffer.vue';
+import JobOffer from '@components/job_offer/JobOffer.vue';
+import Filters from '@components/filters/Filters.vue';
 import OffersService from '@services/OffersService';
 import { Offer } from '@my_types/Offer';
 import { ref, onMounted } from 'vue';
 
-const offers = ref<Offer[]>();
+const offers = ref<Offer[]>([]);
+const skills = ref<string[]>([]);
 
 onMounted(async () => {
-  offers.value = await new OffersService().getAllOffers();
+  const offersService = new OffersService();
+  offers.value = await offersService.getAllOffers();
+  skills.value = await offersService.getAllSkills();
 });
 </script>
 
 <template>
   <main class="search_page">
+    <Filters :skills="skills" />
     <JobOffer v-for="(offer, index) in offers" :key="index" :offer="offer" />
   </main>
 </template>
