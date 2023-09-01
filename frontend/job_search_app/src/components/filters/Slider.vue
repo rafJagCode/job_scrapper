c6b72f590b62e2ad398caa7e6d9131f2d295ad81
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-01 17:55:43 +0200

    Added Slider component

---
+++ frontend/job_search_app/src/components/filters/Slider.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { uuid } from 'vue-uuid';
+const id = uuid.v4();
+</script>
+
+<template>
+  <label class="slider" :for="id">
+    <input class="slider__input" type="checkbox" :id="id" />
+    <div class="slider__background"></div>
+    <div class="slider__ball"></div>
+  </label>
+</template>
+
+<style lang="scss" scoped>
+$slider-width: 3rem;
+.slider {
+  cursor: pointer;
+  position: relative;
+  display: block;
+  width: $slider-width;
+  height: calc($slider-width / 2);
+  background-color: transparent;
+  border-radius: $slider-width;
+  overflow: hidden;
+}
+.slider__input {
+  display: none;
+  &:checked ~ .slider__ball {
+    transform: translateX(125%);
+  }
+  &:checked ~ .slider__background {
+    background-color: $accept;
+  }
+}
+.slider__ball {
+  position: absolute;
+  left: 5%;
+  top: 10%;
+  width: 40%;
+  aspect-ratio: 1;
+  background-color: $light;
+  border-radius: 50%;
+  transition: 400ms;
+}
+.slider__background {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: $light-grey;
+  z-index: -1;
+  transition: 400ms;
+}
+</style>
