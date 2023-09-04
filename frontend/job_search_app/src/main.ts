760621bbcefbd95ad2438f6cb7468892825f1086
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-04 23:06:38 +0200

    Ignored unimportant ts error in main ts

--- frontend/job_search_app/src/main.ts
+++ frontend/job_search_app/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import './scss/reset.scss';
 import './scss/global.scss';
 import withUUID from 'vue-uuid';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import App from './App.vue';
 
 withUUID(createApp(App)).mount('#app');
