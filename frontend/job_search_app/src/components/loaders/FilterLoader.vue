82268bddee310c90186d0145bd3bf0341bed0ea2
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-05 22:25:10 +0200

    Added loaders for offers, skills and cities api requests

---
+++ frontend/job_search_app/src/components/loaders/FilterLoader.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ChevronDownIcon } from '@heroicons/vue/24/outline';
+
+const _props = defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+  <div class="filter_loader">
+    <p class="filter_loader__text">{{ text }}</p>
+    <ChevronDownIcon class="icon md" />
+  </div>
+</template>
+
+<style lang="scss">
+.filter_loader {
+  position: relative;
+  height: 3.5rem;
+  width: 100%;
+  border-bottom: 1px solid $secondary;
+  color: $light;
+  font-size: 1.2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  overflow: hidden;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translateX(calc(-100% - 5rem));
+    height: 100%;
+    width: 10%;
+    background-color: $secondary-light;
+    box-shadow: 0 0 4rem 5rem $secondary-light;
+    animation-duration: 1.5s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    animation-name: loading;
+    animation-timing-function: linear;
+  }
+}
+
+@keyframes loading {
+  to {
+    transform: translateX(calc(1000% + 5rem));
+  }
+}
+</style>
---
+++ frontend/job_search_app/src/components/loaders/OffersLoader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="offers_loader">
+    <div v-for="n in 10" :key="n" class="offers_loader__placeholder"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.offers_loader {
+  width: calc(100vw - $filters-width - 2 * $filters-margin - 2rem);
+  margin: 0 1rem 0 auto;
+  &__placeholder {
+    position: relative;
+    background-color: $secondary;
+    margin: 1rem 1rem 1rem auto;
+    padding: 0.5rem 2rem;
+    height: 7.6rem;
+    width: 100%;
+    border-radius: 1rem;
+    overflow: hidden;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      transform: translateX(calc(-100% - 5rem));
+      height: 100%;
+      width: 10%;
+      background-color: $secondary-light;
+      box-shadow: 0 0 4rem 5rem $secondary-light;
+      animation-duration: 1.5s;
+      animation-fill-mode: forwards;
+      animation-iteration-count: infinite;
+      animation-name: loading;
+      animation-timing-function: linear;
+    }
+  }
+}
+
+@keyframes loading {
+  to {
+    transform: translateX(calc(1000% + 5rem));
+  }
+}
+</style>
