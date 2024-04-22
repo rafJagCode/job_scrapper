98d1f266e45b6883872f6c8275783006238e5aae
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-04-23 01:47:19 +0200

    Changed filtration of offers from has one of filters to has every filter

--- frontend/job_search_app/src/utils/getFilteredOffers.ts
+++ frontend/job_search_app/src/utils/getFilteredOffers.ts
@@ -10,19 +10,24 @@ const getFilteredOffers = (offers: Offer[] | null, filters: Filters) => {
 const getFiltersToApply = (filters: Filters) => {
   const filtersToApply: ((_offer: Offer) => boolean)[] = [];
   if (filters.onlySpecifiedSalary.length > 0) filtersToApply.push(hasSpecifiedSalary);
-  if (filters.workModes.length > 0) filtersToApply.push(hasOneOf(filters, 'workModes'));
-  if (filters.experienceLevels.length > 0) filtersToApply.push(hasOneOf(filters, 'experienceLevels'));
-  if (filters.skills.length > 0) filtersToApply.push(hasOneOf(filters, 'skills'));
-  if (filters.cities.length > 0) filtersToApply.push(hasOneOf(filters, 'cities'));
+  if (filters.workModes.length > 0) filtersToApply.push(hasEvery(filters, 'workModes'));
+  if (filters.experienceLevels.length > 0) filtersToApply.push(hasEvery(filters, 'experienceLevels'));
+  if (filters.skills.length > 0) filtersToApply.push(hasEvery(filters, 'skills'));
+  if (filters.cities.length > 0) filtersToApply.push(hasEvery(filters, 'cities'));
   return filtersToApply;
 };
 
 const hasSpecifiedSalary = (offer: Offer) => Boolean(offer.salary);
 
-const hasOneOf = (filters: Filters, filterName: Exclude<keyof Filters, 'onlySpecifiedSalary'>) => (offer: Offer) =>
-  offer[filterName].some((value) => {
-    const filter: string[] = filters[filterName];
-    return filter.includes(value);
-  });
+const hasEvery = (filters: Filters, filterName: Exclude<keyof Filters, 'onlySpecifiedSalary'>) => (offer: Offer) => {
+  const chosenFiltersValues = filters[filterName];
+  const offerValues = new Set([...offer[filterName]]);
+
+  for (const value of chosenFiltersValues) {
+    if (!offerValues.has(value)) return false;
+  }
+
+  return true;
+};
 
 export default getFilteredOffers;
